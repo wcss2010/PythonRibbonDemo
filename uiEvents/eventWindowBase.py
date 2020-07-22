ae0e69af082e8d5f1883e4f5688794f62b2a946c
wcss2010 <wcss2010@qq.com>
2020-07-22 22:54:11 +0800

    ...

--- uiEvents/eventWindowBase.py
+++ uiEvents/eventWindowBase.py
@@ -3,14 +3,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtNetwork import *
+from uiUtil.envs import *
 import os
 import sys
 import pathlib
 
 '''
-    这是窗体的事件抽象类(类似于C#中的Form类,而uiDefines中的Ui_MainWindow.py则类似于form.designer.cs)
+    这是窗体的事件抽象类(类似于C#中的form.cs类,而uiDefines中的Ui_MainWindow.py则类似于form.designer.cs)
 '''
-class IEventWindow(object):
+class IWindowEvents(object):
     '''
         窗体初始化
     '''
@@ -29,19 +30,63 @@ class IEventWindow(object):
        初始化所有数据(抽象函数)
     '''
     def initUIAndData(self):
-        pass
+        raise NotImplementedError
 
     '''
-        创建窗体线程并进行配置
+       返回UI定义类的实例(例如uiDefines/Ui_MainWindow.py的实例,抽象函数)
     '''
-    def buildWindow(app,parent,ui,event):
-        if app != None and ui != None and event != None:
+    def getUIDefineObject(self):
+        raise NotImplementedError
+
+    '''
+        创建窗体线程并进行配置(基于QMainWindow)
+        输出参数：QMainWindow,窗体UI定义类,窗体事件抽象类
+    '''
+    def buildWindowM(parent,event):
+        uiDefine = event.getUIDefineObject()
+        if cfenv.appObj != None and uiDefine != None and event != None:
             #创建窗体线程
             windowThread = QMainWindow(parent)
             #初始化窗体事件类
-            event.initWindow(app,windowThread,ui)
+            event.initWindow(cfenv.appObj,windowThread,uiDefine)
             #设置描述类到窗体线程
-            ui.setupUi(windowThread)
+            uiDefine.setupUi(windowThread)
+            #返回对象
+            return windowThread,ui,event
+        else:
+            return None,None,None
+
+    '''
+        创建窗体线程并进行配置(基于QWidget)
+        输出参数：QWidget,窗体UI定义类,窗体事件抽象类
+    '''
+    def buildWindowW(parent,event):
+        uiDefine = event.getUIDefineObject()
+        if cfenv.appObj != None and uiDefine != None and event != None:
+            #创建窗体线程
+            windowThread = QWidget(parent)
+            #初始化窗体事件类
+            event.initWindow(cfenv.appObj,windowThread,uiDefine)
+            #设置描述类到窗体线程
+            uiDefine.setupUi(windowThread)
+            #返回对象
+            return windowThread,ui,event
+        else:
+            return None,None,None
+
+    '''
+        创建窗体线程并进行配置(基于QDialog)
+        输出参数：QDialog,窗体UI定义类,窗体事件抽象类
+    '''
+    def buildWindowD(parent,event):
+        uiDefine = event.getUIDefineObject()
+        if cfenv.appObj != None and uiDefine != None and event != None:
+            #创建窗体线程
+            windowThread = QDialog(parent)
+            #初始化窗体事件类
+            event.initWindow(cfenv.appObj,windowThread,uiDefine)
+            #设置描述类到窗体线程
+            uiDefine.setupUi(windowThread)
             #返回对象
             return windowThread,ui,event
         else:
